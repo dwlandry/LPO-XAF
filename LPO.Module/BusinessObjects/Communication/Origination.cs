4b1da0a9e98e517dad83eef10f7d34466eefdcfe
David Landry <dwlandry@gmail.com>
2018-03-19 06:57:06 -0500

    Removed auto-generated comments from Origination.cs file.

--- LPO.Module/BusinessObjects/Communication/Origination.cs
+++ LPO.Module/BusinessObjects/Communication/Origination.cs
@@ -32,20 +32,6 @@ namespace lpmt_xaf.Module.BusinessObjects.Communication
             base.AfterConstruction();
             // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
         }
-        //private string _PersistentProperty;
-        //[XafDisplayName("My display name"), ToolTip("My hint message")]
-        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
-        //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
-        //public string PersistentProperty {
-        //    get { return _PersistentProperty; }
-        //    set { SetPropertyValue("PersistentProperty", ref _PersistentProperty, value); }
-        //}
-
-        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
-        //public void ActionMethod() {
-        //    // Trigger a custom business logic for the current record in the UI (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112619.aspx).
-        //    this.PersistentProperty = "Paid";
-        //}
         string name;
         [Size(255)]
         [Indexed(Name = @"comm_tCommunicationOrigination")]
